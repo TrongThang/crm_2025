ee59dab3962bd65769f4a3de7bd0f6e270bc2993
TrongThang <it.ptthang@gmail.com>
2025-04-09 14:02:31 +0700

    Refactor database connection to use environment variables for DB_HOST and update requirements to correct PyJWT package name

--- crm_app/__init__.py
+++ crm_app/__init__.py
@@ -28,7 +28,7 @@ UPLOAD_FOLDER = 'uploads'
 app.secret_key = '@@#*&Y()P2T@@#*@#$#$%^&*('
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/crm_2025?charset=utf8mb4'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db_host = os.getenv('DB_HOST', 'localhost')  # Mặc định là localhost
+db_host = os.getenv('DB_HOST', 'mysql')  # Mặc định là localhost
 db_password = os.getenv('DB_PASSWORD', '')  # Mặc định là không
 db_user = os.getenv('DB_USER', 'root')  # Mặc định là root
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/crm_2025?charset=utf8mb4'
@@ -38,16 +38,6 @@ app.config['SWAGGER'] = {
     'title': "API CRM APP 2025",
     'uiversion': 3
 }
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# @app.route('/uploads/<path:folder>/<path:filename>')
-# def serve_static(folder, filename):
-#     print("folder:", folder)
-#     print("filename:", filename)
-#     print('path:', (os.path.join(app.config['UPLOAD_FOLDER'], folder), filename))
-#     # return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], 'loai_sp'), filename)
-#     # return send_from_directory('uploads/loai_sp/', filename)
-#     return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], folder), filename)
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -55,3 +45,15 @@ if not os.path.exists(UPLOAD_FOLDER):
 swagger = Swagger(app, template_file='main.yaml')
 # swagger = Swagger(app, template_file='./docs/swaggers/main.yaml')
 db = SQLAlchemy(app=app)
+
+def wait_for_db():
+    while True:
+        try:
+            db.session.execute('SELECT 1')
+            print("MySQL connected")
+            break
+        except Exception as e:
+            print(f"Waiting for MySQL: {e}")
+            time.sleep(1)
+
+wait_for_db()
--- crm_app/helpers/kafka.py
+++ crm_app/helpers/kafka.py
@@ -1,9 +1,21 @@
 from kafka import KafkaProducer, KafkaConsumer
 import os
 import json
+import time
+
 kafka_host = os.getenv('KAFKA_HOST', 'localhost')  # Mặc định là localhost
 bootstrap_servers = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", f"{kafka_host}:9092")
-
+# def init_kafka_producer():
+#     kafka_host = os.getenv('KAFKA_HOST', 'kafka')
+#     bootstrap_servers = f"{kafka_host}:9092"
+#     while True:
+#         try:
+#             producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+#             print("Kafka connected")
+#             return producer
+#         except Exception as e:
+#             print(f"Waiting for Kafka: {e}")
+#             time.sleep(1)
 
 producer = KafkaProducer(
     bootstrap_servers=bootstrap_servers,
